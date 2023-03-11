85094a464e595de012302132a4299c5b4f06def7
hunterdyar <bloop@hdyar.com>
2023-03-11 18:31:40 -0500

    placeholder

---
+++ Assets/Nav Tiles/Scripts/Pathfinding/ThetaStarPathfinding.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nav_Tiles.Scripts.Pathfinding
+{
+	public class ThetaStarPathfinding : IPathfinder
+	{
+		public List<NavNode> FindPath(NavNode start, NavNode finish)
+		{
+			//placeholder
+			//theta*-pathfinding.
+
+			//http://idm-lab.org/bib/abstracts/papers/aaai07a.pdf
+			return new List<NavNode>();
+		}
+	}
+}
