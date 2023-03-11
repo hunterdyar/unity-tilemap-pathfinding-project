679793b39dd288db34028fc0780e086920ea9f8d
hunterdyar <bloop@hdyar.com>
2023-03-11 18:27:33 -0500

    colors reset. nonwalkable tiles tested.

--- Assets/Nav Tiles/Scripts/Example/PathfindingTest.cs
+++ Assets/Nav Tiles/Scripts/Example/PathfindingTest.cs
@@ -9,42 +9,59 @@ namespace Nav_Tiles.Scripts.Example
 	{
 		[SerializeField] private TilemapNavigation _navigation;
 
-		public Vector3Int end;
-		public Vector3Int start;
+		public NavNode endNode;
+		public NavNode startNode;
 
+		private Vector3Int hover;
+		
 		private List<NavNode> tiles = new List<NavNode>();
 		
 		void Update()
 		{
-			var hover = _navigation.Grid.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+			var mouseHover = _navigation.Grid.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
 			if (Input.GetMouseButtonDown(0))
 			{
-				_navigation.Tilemap.SetColor(start, Color.white);
-				start = hover;
-			}
-
-			if (end != hover)
-			{
-				_navigation.Tilemap.SetColor(end,Color.white);
-				
-				end = hover;
-				var startNode = _navigation.GetNavNode(start);
-				var endNode = _navigation.GetNavNode(end);
-				
-				//this is for me in a month when I forget again
-				if (startNode.NavTile.flags == TileFlags.LockColor)
+				if (startNode != null)
 				{
-					Debug.LogWarning("Note: pathfinder testing uses setColor. NavTile flags need to not be set to 'lock color'",startNode.NavTile);
+					_navigation.Tilemap.SetColor(startNode.location, startNode.NavTile.color);
 				}
 
+				startNode = _navigation.GetNavNode(hover);
+			}
+
+			//if we move the mouse to a new space, update.
+			if (hover != mouseHover)
+			{
+				hover = mouseHover;
+				
+				//reset color
+				if (endNode != null)
+				{
+					_navigation.Tilemap.SetColor(endNode.location, endNode.NavTile.color);
+				}
+				
+				//set end to current hover
+				endNode = _navigation.GetNavNode(hover);
+				
+				//if we have start and end nodes.
 				if (startNode != null && endNode != null)
 				{
-					SetColors(Color.white);
+					//this is for me in a month when I forget again
+					if (startNode.NavTile.flags == TileFlags.LockColor)
+					{
+						Debug.LogWarning("Note: pathfinder testing uses setColor. NavTile flags need to not be set to 'lock color'", startNode.NavTile);
+					}
+					
+					ResetColors();
+					
+					//the actual pathfinding. the rest of this script is just faffing about with colors.
 					tiles = _navigation.Pathfinder.FindPath(startNode, endNode);
+					
+					//set all path colors
 					SetColors(Color.blue);
 					//set start and end
-					_navigation.Tilemap.SetColor(start, Color.magenta);
-					_navigation.Tilemap.SetColor(end, Color.green);
+					_navigation.Tilemap.SetColor(startNode.location, Color.magenta);
+					_navigation.Tilemap.SetColor(this.endNode.location, Color.green);
 				}
 			}
 			
@@ -59,5 +76,12 @@ namespace Nav_Tiles.Scripts.Example
 			}
 		}
 
+		private void ResetColors()
+		{
+			foreach (var t in tiles)
+			{
+				_navigation.Tilemap.SetColor(t.location, t.NavTile.color);
+			}
+		}
 	}
 }
