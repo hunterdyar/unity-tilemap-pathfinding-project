e7c5a4c306cfd30448813359d792301a6cd6e2b3
hunterdyar <bloop@hdyar.com>
2023-03-12 14:08:58 -0400

    Remove legacy code from NavTile

--- Assets/Nav Tiles/Scripts/NavTile.cs
+++ Assets/Nav Tiles/Scripts/NavTile.cs
@@ -13,33 +13,7 @@ using UnityEditor;
         public int WalkCost => walkCost;
         [SerializeField] private int walkCost = 0;
         public bool Walkable = true;
-        public Vector3Int Location;
-        public TilemapNavigation TilemapNavigation;
-        //dictionaries are not serialized. so it needs to be initiated. Does this happen?
-        private readonly Dictionary<Vector3Int, NavTile> neighbors = new Dictionary<Vector3Int, NavTile>();
-    
-        public override void RefreshTile(Vector3Int location, ITilemap tilemap)
-        {
-            base.RefreshTile(location,tilemap);
-        }
-       
-        public override void GetTileData(Vector3Int location, ITilemap tilemap, ref TileData tileData)
-        {
-            base.GetTileData(location,tilemap,ref tileData);
-        }
-        // This determines if the Tile at the position is the same RoadTile.
-        private static bool TryGetNavTile(ITilemap tilemap, Vector3Int position, out NavTile navTile)
-        {
-            navTile = null;
-            if (tilemap.GetTile(position) is NavTile tile)
-            {
-                navTile = tile;
-            }
-
-            return false;
-        }
-
-
+        
     
     #if UNITY_EDITOR
     // The following is a helper that adds a menu item to create a RoadTile Asset
