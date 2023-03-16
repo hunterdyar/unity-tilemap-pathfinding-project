14d2789c28773a895fc141a8bca66fb785be0bb9
hunterdyar <bloop@hdyar.com>
2023-03-16 15:12:35 -0400

    First pass at editor for shapes/patterns

---
+++ Assets/Nav Tiles/Scripts/GridShapes/Editor/GridShapeEditor.cs
@@ -0,0 +1,55 @@
+﻿using NavigationTiles.GridShapes;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(GridShape))]
+public class GridShapeEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		var grid = (GridShape)target;
+
+		EditorGUILayout.BeginVertical();
+		
+		var bounds = grid.GridBounds;
+		
+		for (int y = bounds.yMax+1; y >= bounds.yMin-1; y--)
+		{
+			GUILayout.BeginHorizontal();
+			for (int x = bounds.xMin-1; x <= bounds.xMax+1; x++)
+			{
+				bool has = grid.Shape.Contains(new Vector2Int(x, y));
+				bool isOrigin = false;
+				var cell = has ? "X" : " ";
+				GUI.color = Color.gray;
+				
+				if (has)
+				{
+					GUI.color = Color.red;
+				}
+
+				if (x == 0 && y == 0)
+				{
+					isOrigin = true;
+					GUI.color = Color.green;
+					cell = has ? "X" : "0";
+				}
+
+				if (GUILayout.Button(cell, GUILayout.Width(20)))
+				{
+					GUI.enabled = !isOrigin;
+					grid.ToggleCell(new Vector2Int(x,y));
+					GUI.enabled = true;
+				}
+			}
+
+			GUILayout.EndHorizontal();
+		}
+		GUI.color = Color.white;
+
+		EditorGUILayout.EndVertical();
+
+		DrawDefaultInspector();
+	}
+}
---
+++ Assets/Nav Tiles/Scripts/GridShapes/GridShape.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace NavigationTiles.GridShapes
+{
+	[CreateAssetMenu(fileName = "Grid Shape", menuName = "Nav Tiles/Grid Shape", order = 0)]
+	public class GridShape : ScriptableObject
+	{
+		public List<Vector2Int> Shape => _shape;
+		[SerializeField] private List<Vector2Int> _shape;
+		public BoundsInt GridBounds => GetShapeBounds();
+
+		private BoundsInt GetShapeBounds()
+		{
+			BoundsInt bounds = new BoundsInt();
+			int minX = 0;
+			int minY = 0;
+			int maxX = 0;
+			int maxY = 0;
+			foreach (var pos in _shape)
+			{
+				minX = Mathf.Min(minX, pos.x);
+				maxX = Mathf.Max(maxX, pos.x);
+				
+				minY = Mathf.Min(minY, pos.y);
+				maxY = Mathf.Max(maxY, pos.y);
+			}
+			bounds.SetMinMax(new Vector3Int(minX,minY,0),new Vector3Int(maxX,maxY,0));
+			return bounds;
+		}
+
+		public void ToggleCell(Vector2Int pos)
+		{
+
+#if UNITY_EDITOR
+				Undo.RecordObject(this, "Toggle Cell");
+#endif
+				// reassemble
+				if (_shape.Contains(pos))
+				{
+					_shape.Remove(pos);
+				}
+				else
+				{
+					_shape.Add(pos);
+				}
+#if UNITY_EDITOR
+				EditorUtility.SetDirty(this);
+#endif
+		}
+		
+		
+		//Default shape looks up. Looking right, down, left, or up.
+		// public static List<Vector2Int> GetShapeInFacingDirection(Vector2Int facing, bool flipInsteadOfRotate)
+		// {
+		// 	//
+		// 	
+		// }
+	}
+}
